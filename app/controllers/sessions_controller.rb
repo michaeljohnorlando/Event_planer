b25725bc9408fa43fda3ab6babe3c55c1d61664a
Michael Orlando <michael.john.orlando@gmail.com>
2017-08-06 10:53:24 -0700

    cant get session to save in the login controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,60 @@
 class SessionsController < ApplicationController
-  def log_in
-    @session = Session.new
-    @session.login = true
+
+  def params_user
+    params.require(:user).permit(:name, :password)
   end
-  def log_out
-    @session = Session.new
-    @session.login = false
+  def params_session
+    params.require(:session).permit(:session_id, :user_id)
+  end
+
+  def login # did this in the User controller and view...
+    @user = User.new
+  end
+  def login_now
+    @user = User.new(params_user)
+    if User.exists?(name: @user.name)
+      puts " \n the user is in the databace \n"
+      @check = User.find_by_name(@user.name)
+      if @check.authenticate(@user.password)
+        puts "\n the password entered is valid \n"
+
+
+        @session = Session.new
+        # need to make shure there arent more then one
+        # need a timmer to delete automaticly in a day or 2
+        id  = SecureRandom.hex(64)
+        @session.session_id = id
+        @session.user_id    = @user.id
+        puts " save? "
+        @session.save
+        puts " why rollback?"
+
+
+        # Save ID and random temp hash in cookie
+        cookies[:session_id] = id
+        # go to homepage
+        redirect_to '/Events/homepage'
+      else
+        puts "\n the password entered did not match \n "
+        puts "\n Entered: #{@user.password_digest} \n Check:  :#{@check.password_digest}"
+        redirect_to '/users/login'
+      end
+    end
+  end
+
+
+  def logout
+    @user = User.new # this is not really used... need to use a diffrent form?
+  end
+  def logout_now
+    @session = Session.find_by(session_id: cookies[:session_id])
+    if @session
+      @session.destroy
+      cookies.delete :session_id
+      cookies.delete :key
+      redirect_to "/Events/homepage"
+    else
+      "for some reason your session was not found"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   get 'Events/homepage', to: 'thing_to_dos#homepage'
   get  'users/new',      to: 'users#new'
   post 'users/create',   to: 'users#create'
+  get  '/users/logout',  to: 'sessions#logout'
+  post '/users/logout',  to: 'sessions#logout_now'
+  get  '/users/login',   to: 'sessions#login'
+  post '/users/login',   to: 'sessions#login_now'
 end
